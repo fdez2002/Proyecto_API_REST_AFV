da43a670fe339088d34280b04bc79bc16a3f3359
Antonio Fernández Villa <fdez2002@gmail.com>
2023-02-22 13:20:40 +0100

    Controlador productos

---
+++ src/main/java/com/fdez/proyecto_api_rest_afv2/controladores/ControladorProductos.java
@@ -0,0 +1,71 @@
+package com.fdez.proyecto_api_rest_afv2.controladores;
+
+import com.fdez.proyecto_api_rest_afv2.modelo.CategoriasClass;
+import com.fdez.proyecto_api_rest_afv2.modelo.ICategoriasDAO;
+import com.fdez.proyecto_api_rest_afv2.modelo.IProductosDAO;
+import com.fdez.proyecto_api_rest_afv2.modelo.ProductosClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api-rest/productos")
+public class ControladorProductos {
+    @Autowired
+    private IProductosDAO productosDAO;
+
+    @GetMapping
+    public List<ProductosClass> buscarProducto(){return (List<ProductosClass>) productosDAO.findAll();}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductosClass> buscarproductoPorId(
+            @PathVariable(value = "id") int id
+    ){
+        Optional<ProductosClass> categoria = productosDAO.findById(id);
+
+        if(categoria.isPresent()){
+            return ResponseEntity.ok().body(categoria.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ProductosClass guardarProducto(
+            @Validated @RequestBody ProductosClass categoria ){
+        return productosDAO.save(categoria);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrarProducto(
+            @PathVariable(value = "id") int id
+    ){
+        Optional<ProductosClass> categoria = productosDAO.findById(id);
+
+        if (categoria.isPresent()){
+            productosDAO.deleteById(id);
+            return ResponseEntity.ok().body("Borrado");
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductosClass> editarProductoPorId(
+            @PathVariable(value = "id") int id,
+            @Validated @RequestBody ProductosClass productoedit
+    ){
+        Optional<ProductosClass> categoria = productosDAO.findById(id);
+
+        if(categoria.isPresent()){
+            productosDAO.save(productoedit);
+            return ResponseEntity.ok().body(productoedit);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
